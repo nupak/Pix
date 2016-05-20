0a2376cf8dfaeddf38992a4c8f41e0ce22206404
nupak <niki96_@mail.ru>
2016-05-20 17:19:02 +0300

    initial commit

--- src/examples/px4_simple_app/px4_simple_app.c
+++ src/examples/px4_simple_app/px4_simple_app.c
@@ -1,4 +1,4 @@
-/****************************************************************************
+﻿/****************************************************************************
  *
  *   Copyright (c) 2012-2015 PX4 Development Team. All rights reserved.
  *
@@ -37,7 +37,6 @@
  *
  * @author Example User <mail@example.com>
  */
-
 #include <px4_config.h>
 #include <px4_tasks.h>
 #include <px4_posix.h>
@@ -45,14 +44,80 @@
 #include <stdio.h>
 #include <poll.h>
 #include <string.h>
-
+#include<math.h>
 #include <uORB/uORB.h>
-#include <uORB/topics/sensor_combined.h>
+#include <uORB/topics/sensor_accel.h>
 #include <uORB/topics/vehicle_attitude.h>
 
 __EXPORT int px4_simple_app_main(int argc, char *argv[]);
 
 int px4_simple_app_main(int argc, char *argv[])
 {
-    PX4_INFO("DIE FUCKING WORLD!");
-    return OK;}
+    PX4_INFO("Hello Sky!");
+
+    /* subscribe to sensor_combined topic */
+    int sensor_sub_fd = orb_subscribe(ORB_ID(sensor_accel));
+    orb_set_interval(sensor_sub_fd, 1000);
+
+    /* advertise attitude topic */
+    struct vehicle_attitude_s att;
+    memset(&att, 0, sizeof(att));
+    orb_advert_t att_pub = orb_advertise(ORB_ID(vehicle_attitude), &att);
+
+    /* one could wait for multiple topics with this technique, just using one here */
+    px4_pollfd_struct_t fds[] = {
+        { .fd = sensor_sub_fd,   .events = POLLIN },
+        /* there could be more file descriptors here, in the form like:
+         * { .fd = other_sub_fd,   .events = POLLIN },
+         */
+    };
+
+    int error_counter = 0;
+
+    for (int i = 0; i < 5; i++) {
+        /* wait for sensor update of 1 file descriptor for 1000 ms (1 second) */
+        int poll_ret = px4_poll(fds, 1, 1000);
+
+        /* handle the poll result */
+        if (poll_ret == 0) {
+            /* this means none of our providers is giving us data */
+            PX4_ERR("[px4_simple_app] Got no data within a second");
+
+        } else if (poll_ret < 0) {
+            /* this is seriously bad - should be an emergency */
+            if (error_counter < 10 || error_counter % 50 == 0) {
+                /* use a counter to prevent flooding (and slowing us down) */
+                PX4_ERR("[px4_simple_app] ERROR return value from poll(): %d"
+                       , poll_ret);
+            }
+
+            error_counter++;
+
+        } else {
+
+            if (fds[0].revents & POLLIN) {
+                /* obtained data for the first file descriptor */
+                struct sensor_accel_s raw;
+                /* copy sensors raw data into local buffer */
+                orb_copy(ORB_ID(sensor_accel), sensor_sub_fd, &raw);
+                double tanga1=atan(raw.z/raw.x);
+                double tanga2=atan(raw.z/raw.y);
+                PX4_WARN("[px4_simple_app] Accelerometer:\t%8.4f\t%8.4f",
+                      tanga1,tanga2);
+
+              /* set att and publish this information for other apps*/
+                att.roll = raw.x;
+                att.pitch = raw.y;
+                att.yaw = raw.z;
+                orb_publish(ORB_ID(vehicle_attitude), att_pub, &att);
+            }
+
+            /* there could be more file descriptors here, in the form like:
+             * if (fds[1..n].revents & POLLIN) {}
+             */
+        }
+    }
+    PX4_INFO("exiting");
+
+    return 0;
+}
