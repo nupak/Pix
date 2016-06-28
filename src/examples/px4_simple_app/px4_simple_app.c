5df5a20a6219ad725d88058da9de7a96b57cccc9
nupak <niki96_@mail.ru>
2016-06-28 16:45:38 +0300

    initial commit

--- src/examples/px4_simple_app/px4_simple_app.c
+++ src/examples/px4_simple_app/px4_simple_app.c
@@ -1,42 +1,3 @@
-﻿/****************************************************************************
- *
- *   Copyright (c) 2012-2015 PX4 Development Team. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- * 3. Neither the name PX4 nor the names of its contributors may be
- *    used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
- * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
- * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- ****************************************************************************/
-
-/**
- * @file px4_simple_app.c
- * Minimal application example for PX4 autopilot
- *
- * @author Example User <mail@example.com>
- */
 #include <px4_config.h>
 #include <px4_tasks.h>
 #include <px4_posix.h>
@@ -45,41 +6,119 @@
 #include <poll.h>
 #include <string.h>
 #include<math.h>
-#include <uORB/uORB.h>
 #include <uORB/topics/sensor_accel.h>
 #include <uORB/topics/vehicle_attitude.h>
+#include <uORB/uORB.h>
+#include <uORB/topics/sensor_combined.h>
 
-__EXPORT int px4_simple_app_main(int argc, char *argv[]);
-int px4_simple_app_main(int argc, char *argv[])
+static bool thread_should_exit = false;
+static bool thread_running = false;
+static int daemon_task;
+
+__EXPORT int px4_daemon_app_main(int argc, char *argv[]);
+int px4_daemon_thread_main(int argc, char *argv[]);
+static void usage(const char *reason);
+//_____________________________________________________
+static void
+usage(const char *reason)
 {
-    PX4_INFO("Let's start");
-    char c;
-    int sensor_sub_fd = orb_subscribe(ORB_ID(sensor_accel));
-    orb_set_interval(sensor_sub_fd, 1000);
-while ((c = getchar()) != 'q'){ //обязательно через аргументы командной строки? или это тоже вариант?
+    if (reason) {
+        warnx("%s\n", reason);
+    }
 
-                struct sensor_accel_s raw;
+    warnx("usage: daemon {start|stop|status} [-p <additional params>]\n\n");
+}
+//______________________________________________________________________
+int px4_daemon_app_main(int argc, char *argv[])
+{
+    if (argc < 2) {
+        usage("missing command");
+        return 1;
+    }
 
+    if (!strcmp(argv[1], "start")) {
+
+        if (thread_running) {
+            warnx("daemon already running\n");
+            return 0;
+        }
+
+        thread_should_exit = false;
+        daemon_task = px4_task_spawn_cmd("daemon",
+                         SCHED_DEFAULT,
+                         SCHED_PRIORITY_DEFAULT,
+                         2000,
+                         px4_daemon_thread_main,
+                         (argv) ? (char *const *)&argv[2] : (char *const *)NULL);
+        return 0;
+    }
+
+    if (!strcmp(argv[1], "stop")) {
+        thread_should_exit = true;
+        return 0;
+    }
+
+    if (!strcmp(argv[1], "status")) {
+        if (thread_running) {
+            warnx("\trunning\n");
+
+        } else {
+            warnx("\tnot started\n");
+        }
+
+        return 0;
+    }
+
+    usage("unrecognized command");
+    return 1;
+}
+
+int px4_daemon_thread_main(int argc, char *argv[])
+{
+
+    warnx("[daemon] starting\n");
+
+    thread_running = true;
+    int sensor_sub_fd = orb_subscribe(ORB_ID(sensor_combined));
+    px4_pollfd_struct_t fds[] = {
+            { .fd = sensor_sub_fd,   .events = POLLIN }
+        };
+    struct sensor_accel_s raw;
+    while (!thread_should_exit) {
+
+    int error_counter = 0;
+
+
+        int poll_ret = px4_poll(fds, 1, 1000);
+
+
+        if (poll_ret == 0) {
+
+            PX4_ERR("[px4_simple_app] Got no data within a second");
+
+        } else if (poll_ret < 0) {
+
+            if (error_counter < 10 || error_counter % 50 == 0) {
+
+                PX4_ERR("[px4_simple_app] ERROR return value from poll(): %d"
+                       , poll_ret);
+            }
+
+            error_counter++;
+
+        } else
+        {
+
+            if (fds[0].revents & POLLIN) {
                 orb_copy(ORB_ID(sensor_accel), sensor_sub_fd, &raw);
                 double a1,a2,osx,osy,osz;
-
-                 osx=raw.x; osy=raw.y; osz=raw.z;
-                 if (osx<0)
-                      if (osz>=0) a1=30;
-                      else a1 =-30;
-                 else  a1=atan(osz/osx);
-                      if (a1>30) a1=30;//если угол
-                      else             //вне диапазона [-30;30]
-                        if (a1<-30) a1=-30;
-                 // ///////////////////////////////////////////////
-                 if (osy<0)
-                      if (osz>0) a2=30;
-                       else a2=-30;
-                 else  a2=atan(osz/osy);
-                    if (a2>30) a2=30;             //если угол
-                         else  if (a2<-30) a2=-30;//вне диапазона [-30;30]
-
-                PX4_WARN("[px4_simple_app] Accelerometer:\t%8.4f\t%8.4f",
-                      a1,a2);}
-    return 0;
+        osx=raw.x; osy=raw.y; osz=raw.z;
+                a1=atan2(osx,sqrt(osy*osy+osz*osz));
+        a2=atan2(osy,sqrt(osx*osx+osz*osz));
+        PX4_WARN("[px4_simple_app] a1\t%8.4f a2\t%8.4f",
+                              a1,a2);
+       }
+    }
+}
+return 0;
 }
